e3f47c19d699b9687d4d3161fcf422d917c2bcca
maisaroq <mais209@gmail.com>
2021-05-24 17:49:38 +0200

    step 1 done models

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,15 @@
+class ListingsController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :listings
   devise_for :users
   root to: 'pages#home'
+  resources :listings, only: [:index, :new, :create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
