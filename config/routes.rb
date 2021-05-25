3adf71f31daedd575c2d6662d8939787de4575e4
Vojtech Matous <vojtech.matous@gmail.com>
2021-05-25 16:35:15 +0200

    show one listing added gemlock version fixed

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :listings, only: [ :index, :new, :create, :show ] do
-  end
+  resources :listings, only: [ :index, :new, :create, :show ]
 
+  # resources :listings, only: [ :index, :new, :create, :show ] do
+  #   resources :listings, only: [:show]
+  # end
 end
