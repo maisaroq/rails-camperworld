d29911e1015e5873556f789ac7239c46c22e93e7
Vojtech Matous <vojtech.matous@gmail.com>
2021-05-26 15:56:53 +0200

    search without style added seed still not working

--- db/seeds.rb
+++ db/seeds.rb
@@ -28,12 +28,13 @@ puts ""
 puts "---Creating 20 fake products..."
 20.times do
   listing = Listing.new(
+    location: ["Berlin", "Prague", "paris"].sample,
     name: Faker::Commerce.product_name,
     description: Faker::Lorem.paragraph,
     price: Faker::Commerce.price,
     equipment_type: ["Tent", "Campervan", "Cooker", "Survival Kit", "Backpack"].sample,
     image: "https://source.unsplash.com/random",
-    user_id: rand(1..10)
+    user_id: rand(16..25)
   )
   puts listing.name
   listing.save!
