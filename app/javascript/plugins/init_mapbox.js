14244b385f89692e823542be44b5da72225561e0
Emily Hoult <emily@emilyhoult.com>
2021-05-27 16:55:27 +0200

    add mapbox to js files

---
+++ app/javascript/plugins/init_mapbox.js
@@ -0,0 +1,16 @@
+import mapboxgl from 'mapbox-gl';
+import 'mapbox-gl/dist/mapbox-gl.css';
+
+const initMapbox = () => {
+  const mapElement = document.getElementById('map');
+
+  if (mapElement) {
+    mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
+    const map = new mapboxgl.Map({
+      container: 'map',
+      style: 'mapbox://styles/mapbox/streets-v10'
+    });
+  }
+};
+
+export { initMapbox };
