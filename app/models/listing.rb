55b50552e247263c46a9cf0e7b4199d473918c15
Hannah Eichelsdoerfer <hannaheichelsdoerfer@gmail.com>
2021-05-27 15:20:05 +0200

    test

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,7 +2,10 @@ class Listing < ApplicationRecord
   belongs_to :user
   has_one_attached :photo
 
+  TYPE = ["van", "tent", "cooking supplies", "technology", "other"]
+
   validates :name, :price, presence: true
   validates :description, presence: :true, length: { minimum: 50 }
   validates :location, presence: true
+  # validates :equipment_type, inclusion: { in: TYPE }
 end
